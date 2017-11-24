12c3668d79537ea6fdf35653c67418f9922b5384
Dmytro Mykhalyk <d.mykhalyk@gmail.com>
2017-11-24 17:20:14 +0200

    fix dup for PostgresReadyChecker

--- samples/src/main/scala/com/whisk/docker/DockerPostgresServiceLatest.scala
+++ samples/src/main/scala/com/whisk/docker/DockerPostgresServiceLatest.scala
@@ -5,14 +5,14 @@ import java.sql.DriverManager
 import scala.concurrent.ExecutionContext
 import scala.util.Try
 
-trait DockerPostgresService extends DockerKit {
+trait DockerPostgresServiceLatest extends DockerKit {
   import scala.concurrent.duration._
   def PostgresAdvertisedPort = 5432
-  def PostgresExposedPort = 44444
-  val PostgresUser = "nph"
-  val PostgresPassword = "suitup"
+  def PostgresExposedPort = 5444
+  val PostgresUser = "postgres"
+  val PostgresPassword = "postgres"
 
-  val postgresContainer = DockerContainer("postgres:9.5.3")
+  val postgresContainer = DockerContainer("postgres:latest")
     .withPorts((PostgresAdvertisedPort, Some(PostgresExposedPort)))
     .withEnv(s"POSTGRES_USER=$PostgresUser", s"POSTGRES_PASSWORD=$PostgresPassword")
     .withReadyChecker(
@@ -24,17 +24,3 @@ trait DockerPostgresService extends DockerKit {
     postgresContainer :: super.dockerContainers
 }
 
-class PostgresReadyChecker(user: String, password: String, port: Option[Int] = None)
-    extends DockerReadyChecker {
-
-  override def apply(container: DockerContainerState)(implicit docker: DockerCommandExecutor,
-                                                      ec: ExecutionContext) =
-    container
-      .getPorts()
-      .map(ports =>
-        Try {
-          Class.forName("org.postgresql.Driver")
-          val url = s"jdbc:postgresql://${docker.host}:${port.getOrElse(ports.values.head)}/"
-          Option(DriverManager.getConnection(url, user, password)).map(_.close).isDefined
-        }.getOrElse(false))
-}
--- scalatest/src/test/scala/com/whisk/docker/PostgresServiceLatestSpec.scala
+++ scalatest/src/test/scala/com/whisk/docker/PostgresServiceLatestSpec.scala
@@ -6,18 +6,18 @@ import com.whisk.docker.scalatest.DockerTestKit
 import org.scalatest.time.{Second, Seconds, Span}
 import org.scalatest.{FlatSpec, Matchers}
 
-class PostgresServiceSpec
+class PostgresServiceLatestSpec
     extends FlatSpec
     with Matchers
     with DockerTestKit
-    with DockerPostgresService {
+    with DockerPostgresServiceLatest {
 
   implicit val pc = PatienceConfig(Span(20, Seconds), Span(1, Second))
 
   override implicit val dockerFactory: DockerFactory = new SpotifyDockerFactory(
     DefaultDockerClient.fromEnv().build())
 
-  "postgres node" should "be ready with log line checker" in {
+  "postgres node:latest" should "be ready with log line checker" in {
     isContainerReady(postgresContainer).futureValue shouldBe true
   }
 }
